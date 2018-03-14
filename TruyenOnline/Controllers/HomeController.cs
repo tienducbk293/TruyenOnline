8f2acec1efa74ed752e8033666ed88a4bddffe1a
tienducbk293 <tienducbk293@gmail.com>
2018-03-15 00:25:40 +0700

    Add Novel-Page

--- TruyenOnline/Controllers/HomeController.cs
+++ TruyenOnline/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TruyenOnline.Models;
 
 namespace TruyenOnline.Controllers
 {
     public class HomeController : Controller
     {
+        TruyenOnlineEntities db = new TruyenOnlineEntities();
         public ActionResult Index()
         {
+            string Chuoi = "";
+            var novel = (from p in db.AnhHungXaDieux orderby p.ID descending select p).Take(1).ToList();
+            for (int i = 0; i < novel.Count; i++)
+            {
+                Chuoi += "<h2 style = \"text-align:center\" ><b>" + novel[i].ChapName + "</b></h2>";
+                Chuoi += "<pre style = \"font-family:Times New Roman; font-size:20px\">" + novel[i].Content +"</pre>";
+            }
+            ViewBag.View = Chuoi;
             return View();
         }
 
